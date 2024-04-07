4e9279b2e78a29cee57bd50f04a59fbfa6bb1ecd
Brian Pereboom <52140839+brianpereboom@users.noreply.github.com>
2024-04-07 15:48:21 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 # Imports
-  # yfinance
+  # requests (for alphavantage API)
 
 # Reactive Aspects 
-  # Use option_chain() and history() to get current stock and option data
-  # option_chain() will be called once per minute
+  # Use history() to get current stock data
   # history() will be called when inputs change
 
 # UI Page Inputs
@@ -13,10 +12,7 @@
 # UI Sidebar Components
   # express.ui.input_text()
   # express.ui.input_checkbox_group()
-  # express.ui.input_selectize()
 
 # UI Main Content
-  # express.ui.layout_columns()
   # express.ui.card()
   # Line plot or candlestick chart for stock history
-  # Scatter plot for option data
