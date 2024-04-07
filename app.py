f8aa7c755d118ca97a18cf6a0bed9b44b3fc4073
Brian Pereboom <52140839+brianpereboom@users.noreply.github.com>
2024-04-07 13:32:59 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+# Imports
+  # yfinance
+
+# Reactive Aspects 
+  # Use option_chain() and history() to get current stock and option data
+  # option_chain() will be called once per minute
+  # history() will be called when inputs change
+
+# UI Page Inputs
+  # Choose stock ticker
+  # Choose indicators and time frames
+
+# UI Sidebar Components
+  # express.ui.input_text()
+  # express.ui.input_checkbox_group()
+  # express.ui.input_selectize()
+
+# UI Main Content
+  # express.ui.layout_columns()
+  # express.ui.card()
+  # Line plot or candlestick chart for stock history
+  # Scatter plot for option data
